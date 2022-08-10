0791b3c787028ba165e80c04244eedc565c1c467
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-08-10 13:46:09 -0300

    feat: :white_check_mark: Requisito 9 finalizado.
    Foi criada a classe Monster, e foi refatorado a classe Character para ter as opções de atacar os monstros e o lutadores.

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -45,7 +45,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
@@ -61,7 +61,7 @@ export default class Character implements Fighter {
     this._lifePoints = this._maxLifePoints;
   }
 
-  special(enemy: Fighter): void {
+  special(enemy: Fighter | SimpleFighter): void {
     if (this._energy.amount > 4) {
       enemy.receiveDamage(this._dexterity * 5);
       this._energy.amount -= 5;
